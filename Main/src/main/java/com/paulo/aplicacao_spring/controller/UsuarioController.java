348e895ca8de17cb8e3aed049bf7e274c73ca619
paulorpb <paulo10renato@gmail.com>
2026-03-12 17:08:05 -0300

    correções 12/03/26

--- Main/src/main/java/com/paulo/aplicacao_spring/controller/UsuarioController.java
+++ Main/src/main/java/com/paulo/aplicacao_spring/controller/UsuarioController.java
@@ -1,6 +1,8 @@
 package com.paulo.aplicacao_spring.controller; 
 
 import com.paulo.aplicacao_spring.model.Usuario;
+import com.paulo.aplicacao_spring.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +12,28 @@ import java.util.List;
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
-    private List<Usuario> usuarios = new ArrayList<>();
+    @Autowired
+    private UsuarioRepository repository;
+
+    @GetMapping
+    public List<Usuario> listar() {
+        return repository.findAll(); // Busca no H2
+    }
+
+    @PostMapping
+    public String adicionar(@RequestBody Usuario user) {
+        repository.save(user); // Salva no H2
+        return "Usuário " + user.getNome() + " cadastrado com sucesso!";
+    }
+
+    // Melhore o DELETE para usar o ID do banco em vez do índice da lista
+    @DeleteMapping("/{id}")
+    public String deletar(@PathVariable Long id) {
+        repository.deleteById(id);
+        return "Usuário removido!";
+    }
+
+    /*private final List<Usuario> usuarios = new ArrayList<>();
 
     @GetMapping
     public List<Usuario> listar() {
@@ -42,5 +65,5 @@ public class UsuarioController {
         return "Usuário " + removido.getNome() + " removido com sucesso!";
     }
     return "Erro: Não foi possível encontrar o usuário no índice " + index;
-}
+}*/
 }
--- Main/src/main/java/com/paulo/aplicacao_spring/model/Usuario.java
+++ Main/src/main/java/com/paulo/aplicacao_spring/model/Usuario.java
@@ -1,25 +1,22 @@
 package com.paulo.aplicacao_spring.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Entity // Diz que isso é uma tabela
+@Table(name = "usuarios")
+@Getter @Setter @NoArgsConstructor // Aproveitando o Lombok que você já tem no pom
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // O banco gerenciará este ID agora
+
     private String nome;
 
-    // Construtor vazio (Necessário para o Spring)
-    public Usuario() {
-    }
-
     public Usuario(String nome) {
-        this.nome = nome;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void putNome(String nome){
         this.nome = nome;
     }
 }
